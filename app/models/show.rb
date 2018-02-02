9d20c51b8c0f60dba445dd6cbdc16316e41081bc
Andy Maskiell <maskiella@gmail.com>
2018-02-02 19:27:26 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,2 +1,28 @@
 class Show < ActiveRecord::Base
+
+  def self.highest_rating
+    self.maximum(:rating)
+  end
+
+  def self.most_popular_show
+    self.maximum(:rating).name
+  end
+
+  def self.lowest_rating
+    self.minimum(:rating)
+  end
+
+  def self.least_popular_show
+    self.minimum(:rating)
+  end
+
+  def self.ratings_sum
+    self.sum(:rating)
+  end
+
+  def self.popular_shows
+  end
+
+  def self.shows_by_alphabetical_order
+  end
 end
